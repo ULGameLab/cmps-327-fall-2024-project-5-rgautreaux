1dfb2ee870042c5260abc705e30cb3acb01bb35d
rgautreaux <C00460817@louisiana.edu>
2024-11-03 17:41:47 -0600

    A* Path Following Implementation
    After reviewing class slides and watching the A* Explanation vide on Moodle, I tried my best to apply the knowledge I gained to the Pathfinding Task in the instructions.  There appear to be a couple issues stemming from functions not recognizing each other, so I will continue to work on it at a different date.

--- Assets/Scripts/PathFinder.cs
+++ Assets/Scripts/PathFinder.cs
@@ -3,19 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 using System;
 using MapGen;
+using UnityEditor.TerrainTools;
+using System.Linq;
+using UnityEngine.UIElements;
+using NUnit.Framework;
 
 public class Node
 {
     public Node cameFrom = null; //parent node
-    public double priority = 0; // F value
-    public double costSoFar = 0; // G Value
+    public double priority = 0; // F value (current estimated cost for point n)
+    public double costSoFar = 0; // G Value (distance from the start point to point n)
+    public double remainingDist = 0; // H value (estimated distance from point n to the goal point)
+
     public Tile tile;
 
-    public Node(Tile _tile, double _priority, Node _cameFrom, double _costSoFar)
+    public Node(Tile _tile, double _priority, Node _cameFrom, double _costSoFar, double _remainingDist)
     {
         cameFrom = _cameFrom;
         priority = _priority; 
         costSoFar = _costSoFar;
+        remainingDist = _remainingDist;
         tile = _tile;
     }
 }
@@ -24,7 +31,7 @@ public class PathFinder
 {
     List<Node> TODOList = new List<Node>();
     List<Node> DoneList = new List<Node>();
-    Tile goalTile; 
+    Tile goalTile;
 
 
     // This is the constructor
@@ -38,8 +45,9 @@ public class PathFinder
     {
         TODOList = new List<Node>();
         DoneList = new List<Node>();
+        //Neighbors = new List<Node>();
 
-        TODOList.Add(new Node(start, 0, null, 0));
+        TODOList.Add(new Node(start, 0, null, 0, 0));
         goalTile = goal;
 
         while (TODOList.Count > 0)
@@ -58,7 +66,40 @@ public class PathFinder
             // You just need to fill code inside this foreach only
             foreach (Tile nextTile in current.tile.Adjacents)
             {
-                
+                Node neighbor = TODOList[1];
+
+                //if this neighboring tile is not traversable or
+                //closed/done you SKIP to the next neighboring tile
+                if (DoneList.Contains(neighbor) || !current.tile.Adjacents.Contains(neighbor))
+                {
+                    //Move on to next neighbor
+                    DoneList.Add(neighbor);
+                    TODOList.RemoveAt(1);
+                }
+
+                //if the new path to this neighbor tile is shorter OR
+                //this neighbor is not available/open, then edit the f
+                //cost and path accordingly
+
+                double oldPath = TileDistance(current.tile, neighbor.tile);
+                DoneList.Add(neighbor);
+                TODOList.RemoveAt(1);
+                double newPath = TileDistance(current.tile, neighbor.tile);
+
+                if (newPath < oldPath || !TODOList.Contains(neighbor))
+                {
+                    //set F Cost of neighbor
+                    neighbor.priority = HeuristicsDistance(current.tile, neighbor.tile);
+
+                    //set parent of neighor to current
+                    neighbor.cameFrom = current;
+
+                    //add available/open nodes to TODO List
+                    if (!TODOList.Contains(neighbor))
+                    {
+                        DoneList.Add(neighbor);
+                    }
+                }
             }
         }
         return new Queue<Tile>(); // Returns an empty Path if no path is found
@@ -72,7 +113,7 @@ public class PathFinder
         TODOList = new List<Node>();
         DoneList = new List<Node>();
 
-        TODOList.Add(new Node(start, 0, null, 0));
+        TODOList.Add(new Node(start, 0, null, 0, 0));
         goalTile = goal;
 
         while (TODOList.Count > 0)
@@ -92,86 +133,145 @@ public class PathFinder
             // Just increase the F cost of the enemy tile and the tiles around it by a certain ammount (say 30)
             foreach (Tile nextTile in current.tile.Adjacents)
             {
+                Node neighbor = TODOList[1];
 
+                //if this neighboring tile is not traversable or
+                //closed/done you SKIP to the next neighboring tile
+                if (DoneList.Contains(neighbor) || !current.tile.Adjacents.Contains(neighbor))
+                {
+                    //Move on to next neighbor
+                    DoneList.Add(neighbor);
+                    TODOList.RemoveAt(1);
+                }
+
+                //if the new path to this neighbor tile is shorter OR
+                //this neighbor is not available/open, then edit the f
+                //cost and path accordingly
+
+                double oldPath = TileDistance(current.tile, neighbor.tile);
+                DoneList.Add(neighbor);
+                TODOList.RemoveAt(1);
+                double newPath = TileDistance(current.tile, neighbor.tile);
+
+                if (newPath < oldPath || !TODOList.Contains(neighbor))
+                {
+                    //set F Cost of neighbor (HIGHER than usual)
+                    neighbor.priority = EnemyDistance(current.tile, neighbor.tile);
+
+                    //set parent of neighor to current
+                    neighbor.cameFrom = current;
+
+                    //add available/open nodes to TODO List
+                    if (!TODOList.Contains(neighbor))
+                    {
+                        DoneList.Add(neighbor);
+                    }
+
+                }
             }
+            return new Queue<Tile>(); // Returns an empty Path
         }
-        return new Queue<Tile>(); // Returns an empty Path
-    }
 
-    // Manhattan Distance with horizontal/vertical cost of 10
-    double HeuristicsDistance(Tile currentTile, Tile goalTile)
-    {
-        int xdist = Math.Abs(goalTile.indexX - currentTile.indexX);
-        int ydist = Math.Abs(goalTile.indexY - currentTile.indexY);
-        // Assuming cost to move horizontally and vertically is 10
-        //return manhattan distance
-        return (xdist * 10 + ydist * 10);
-    }
-
-    // Retrace path from a given Node back to the start Node
-    Queue<Tile> RetracePath(Node node)
-    {
-        List<Tile> tileList = new List<Tile>();
-        Node nodeIterator = node;
-        while (nodeIterator.cameFrom != null)
+        // Manhattan Distance with horizontal/vertical cost of 10
+        double HeuristicsDistance(Tile currentTile, Tile goalTile)
         {
-            tileList.Insert(0, nodeIterator.tile);
-            nodeIterator = nodeIterator.cameFrom;
+            int xdist = Math.Abs(goalTile.indexX - currentTile.indexX);
+            int ydist = Math.Abs(goalTile.indexY - currentTile.indexY);
+            // Assuming cost to move horizontally and vertically is 10
+            //return manhattan distance
+            return (xdist * 10 + ydist * 10);
         }
-        return new Queue<Tile>(tileList);
-    }
 
-    // Generate a Random Path. Used for enemies
-    public Queue<Tile> RandomPath(Tile start, int stepNumber)
-    {
-        List<Tile> tileList = new List<Tile>();
-        Tile currentTile = start;
-        for (int i = 0; i < stepNumber; i++)
+
+        // Manhattan Distance with horizontal/vertical cost of 30 due to
+        // enemy presence, different from Heuristic Distance
+        double EnemyDistance(Tile currentTile, Tile goalTile)
         {
-            Tile nextTile;
-            //find random adjacent tile different from last one if there's more than one choice
-            if (currentTile.Adjacents.Count < 0)
+            int xdist = Math.Abs(goalTile.indexX - currentTile.indexX);
+            int ydist = Math.Abs(goalTile.indexY - currentTile.indexY);
+            // The cost to move horizontally and vertically is 30
+            // when an enemy is present, return corresponding distance
+            return (xdist * 30 + ydist * 30);
+        }
+
+
+        // Calculate distance between current and neighbor
+        double TileDistance(Tile currentTile, Tile goalTile)
+        {
+            int xdist = Math.Abs(goalTile.indexX - currentTile.indexX);
+            int ydist = Math.Abs(goalTile.indexY - currentTile.indexY);
+            // Assuming cost to move horizontally and vertically is 10
+            //return manhattan distance
+            return (xdist + ydist);
+        }
+
+        // Retrace path from a given Node back to the start Node
+        Queue<Tile> RetracePath(Node node)
+        {
+            List<Tile> tileList = new List<Tile>();
+            Node nodeIterator = node;
+            while (nodeIterator.cameFrom != null)
             {
-                break;
+                tileList.Insert(0, nodeIterator.tile);
+                nodeIterator = nodeIterator.cameFrom;
             }
-            else if (currentTile.Adjacents.Count == 1)
+            return new Queue<Tile>(tileList);
+        }
+
+
+
+        // Generate a Random Path. Used for enemies
+        public Queue<Tile> RandomPath(Tile start, int stepNumber)
+        {
+            List<Tile> tileList = new List<Tile>();
+            Tile currentTile = start;
+            for (int i = 0; i < stepNumber; i++)
             {
-                nextTile = currentTile.Adjacents[0];
-            }
-            else
-            {
-                nextTile = null;
-                List<Tile> adjacentList = new List<Tile>(currentTile.Adjacents);
-                ShuffleTiles<Tile>(adjacentList);
-                if (tileList.Count <= 0) nextTile = adjacentList[0];
+                Tile nextTile;
+                //find random adjacent tile different from last one if there's more than one choice
+                if (currentTile.Adjacents.Count < 0)
+                {
+                    break;
+                }
+                else if (currentTile.Adjacents.Count == 1)
+                {
+                    nextTile = currentTile.Adjacents[0];
+                }
                 else
                 {
-                    foreach (Tile tile in adjacentList)
+                    nextTile = null;
+                    List<Tile> adjacentList = new List<Tile>(currentTile.Adjacents);
+                    ShuffleTiles<Tile>(adjacentList);
+                    if (tileList.Count <= 0) nextTile = adjacentList[0];
+                    else
                     {
-                        if (tile != tileList[tileList.Count - 1])
+                        foreach (Tile tile in adjacentList)
                         {
-                            nextTile = tile;
-                            break;
+                            if (tile != tileList[tileList.Count - 1])
+                            {
+                                nextTile = tile;
+                                break;
+                            }
                         }
                     }
                 }
+                tileList.Add(currentTile);
+                currentTile = nextTile;
             }
-            tileList.Add(currentTile);
-            currentTile = nextTile;
+            return new Queue<Tile>(tileList);
         }
-        return new Queue<Tile>(tileList);
-    }
 
-    private void ShuffleTiles<T>(List<T> list)
-    {
-        // Knuth shuffle algorithm :: 
-        // courtesy of Wikipedia :) -> https://forum.unity.com/threads/randomize-array-in-c.86871/
-        for (int t = 0; t < list.Count; t++)
+        private void ShuffleTiles<T>(List<T> list)
         {
-            T tmp = list[t];
-            int r = UnityEngine.Random.Range(t, list.Count);
-            list[t] = list[r];
-            list[r] = tmp;
+            // Knuth shuffle algorithm :: 
+            // courtesy of Wikipedia :) -> https://forum.unity.com/threads/randomize-array-in-c.86871/
+            for (int t = 0; t < list.Count; t++)
+            {
+                T tmp = list[t];
+                int r = UnityEngine.Random.Range(t, list.Count);
+                list[t] = list[r];
+                list[r] = tmp;
+            }
         }
     }
 }
